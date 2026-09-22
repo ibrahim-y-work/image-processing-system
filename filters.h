686ed8f71d0a1a926536bbd3f50a68a99d622d17
Ibrahim Yasser <ibrahim.y.work@gmail.com>
2026-09-22 18:47:40 +0300

    divide the assignment

--- filters.h
+++ filters.h
@@ -5,61 +5,80 @@
 #ifndef IMAGE_PROCESSING_SYSTEM_FILTERS_H
 #define IMAGE_PROCESSING_SYSTEM_FILTERS_H
 
-
+//ibrahim Abdul-Wahab ->1
 void grayscaleConversion(Image& img) {
 
-}
 
+}
+//ibrahim Yasser ->2
 void  blackAndWhite(Image& img) {
 
 }
-
+//Toqa ->3
 void  invertImage(Image& img) {
 
 }
-
+//Hatem ->4
 void  addingAFrameToThePicture(Image& img) {
 
 }
 
+//ibrahim Abdul-Wahab ->5
 void flipImage(Image& img) {
 
 }
 
+//ibrahim Yasser ->6
 void  rotateImage(Image& img) {
 
 }
 
+//Toqa ->7
 void darkenAndLightenImage(Image& img,bool dark,short levelOfLight) {
 
 }
+//Hatem ->8
+void resizingImages(Image& img) {
 
+}
+//Ibrahim Abdul-Wahab ->9
 void mergeImages(Image& img) {
 
 }
-
+//ibrahim Yasser -> 10
 void detectImageEdges(Image& img) {
 
 }
 
+//Toqa ->11
 void cropImages(Image& img) {
 
 }
+
+//Hatem ->12
 void blurImages(Image& img) {
 
 }
+//ibrahim Abdul-Wahab ->13
 void  naturalSunlight(Image& img) {
 
 }
+
+//Ibrahim Yasser ->14
 void  TVImages(Image& img) {
 
 }
+
+//Toqa->15
 void convertToPurple(Image& img) {
 
 }
+
+//Hatem -> 16
 void convertToInfrared(Image& img) {
 
 }
+
 
 void imageSkewing(Image& img) {
 
